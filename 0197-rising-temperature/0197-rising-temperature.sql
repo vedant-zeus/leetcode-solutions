833795a81c8ab24039cd905dd859aac1fcad0be4
Vedant  Amrutkar <142170238+vedant-zeus@users.noreply.github.com>
2026-09-13 23:21:55 +0530

    Time: 1086 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,5 +1,7 @@
-SELECT  w2.id 
-from Weather as w1
-JOIN Weather as w2 
-    ON DATEDIFF(w2.recordDate,w1.recordDate) = 1
-Where w1.temperature < w2.temperature
+SELECT today.id
+FROM Weather yesterday 
+CROSS JOIN Weather today
+
+WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
+    AND today.temperature > yesterday.temperature
+;
